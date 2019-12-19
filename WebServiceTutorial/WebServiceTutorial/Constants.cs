12256c28a98b10965b8daa72561bc1bd7dd6f514
xamarin <xamarin@rosselli.loc>
2019-12-19 12:37:29 +0100

    Aggiungere i file di progetto.

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServiceTutorial
+{
+    class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "c49313802d25cc64189e0bdd571dcba6";
+    }
+}
